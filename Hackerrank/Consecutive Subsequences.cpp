c0e715cc2dd065e787d78a7d891f9654b3d6b2c5
DivyaAgarw9140 <130404704+DivyaAgarw9140@users.noreply.github.com>
2025-05-12 19:19:24 +0530

    Create Consecutive Subsequences.cpp

---
+++ Hackerrank/Consecutive Subsequences.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        long pre[n],F[n],i;
+        for(i=0;i<n;i++)
+            cin>>F[i];
+        pre[0] = F[0];
+        for(i=1;i<n;i+=1)
+            pre[i] = F[i] + pre[i-1];
+        int count[111]= {0};
+        count[0] = 1;
+        for(i=0;i<n;i++)
+            count[pre[i]%k]++;
+        long sum=0;
+        for(i=0;i<=110;i++)
+            sum += count[i]*(count[i] -1)/2;
+        cout<<sum<<endl;
+    }
+    return 0;
+}
